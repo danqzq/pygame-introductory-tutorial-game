e6139e1b82290f68c97b4f51e7c7fbf9e1df825a
Dan <jumagaliyevj2004@gmail.com>
2022-11-16 18:36:35 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -4,63 +4,58 @@ pygame.init()
 
 # Constants
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
 WINDOW_SIZE = (1280, 720)
 WINDOW_TITLE = "Pygame Tutorial"
 
-FRAME_RATE = 144
+FRAME_RATE = 60
 
-PLAYER_SIZE = (72, 72)
-
-window = pygame.display.set_mode(WINDOW_SIZE)
+WINDOW = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption(WINDOW_TITLE)
-clock = pygame.time.Clock()
 
-player_x = WINDOW_SIZE[0] / 2 - PLAYER_SIZE[0] / 2
-player_y = WINDOW_SIZE[1] / 2 - PLAYER_SIZE[1] / 2
-player_input = {"left": False, "right": False, "up": False, "down": False}
+CLOCK = pygame.time.Clock()
+
+player_x = WINDOW_SIZE[0] / 2
+player_y = WINDOW_SIZE[1] / 2
+player_input = {"left": False, "right": False, "up": False, "down":False}
 player_velocity = [0, 0]
 player_speed = 5
 
 
 def check_input(key, value):
-    if key == pygame.K_a:
+    if key == pygame.K_LEFT or key == pygame.K_a:
         player_input["left"] = value
-    elif key == pygame.K_d:
+    elif key == pygame.K_RIGHT or key == pygame.K_d:
         player_input["right"] = value
-    elif key == pygame.K_w:
+    elif key == pygame.K_UP or key == pygame.K_w:
         player_input["up"] = value
-    elif key == pygame.K_s:
+    elif key == pygame.K_DOWN or key == pygame.K_s:
         player_input["down"] = value
 
 
-def handle_event(evt):
-    if evt.type == pygame.QUIT:
-        exit()
-    elif evt.type == pygame.KEYDOWN:
-        check_input(evt.key, True)
-    elif evt.type == pygame.KEYUP:
-        check_input(evt.key, False)
-
-
-# Game loops
 while True:
     for event in pygame.event.get():
-        handle_event(event)
+        if event.type == pygame.QUIT:
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            check_input(event.key, True)
+        elif event.type == pygame.KEYUP:
+            check_input(event.key, False)
 
-    player_velocity = [0, 0]
-    player_velocity[0] = (player_input["right"] - player_input["left"]) * player_speed
-    player_velocity[1] = (player_input["down"] - player_input["up"]) * player_speed
+    player_velocity[0] = player_input["right"] - player_input["left"]
+    player_velocity[1] = player_input["down"] - player_input["up"]
 
-    window.fill(WHITE)
+    WINDOW.fill(WHITE)
 
-    pygame.draw.rect(window, RED, (0, 0, 100, 100))
-    pygame.draw.circle(window, BLACK, (player_x, player_y), 50)
+    pygame.draw.rect(WINDOW, RED, (600, 600, 200, 100))
 
-    player_x += player_velocity[0]
-    player_y += player_velocity[1]
+    # Player
+    pygame.draw.circle(WINDOW, BLACK, (player_x, player_y), 50)
 
-    clock.tick(FRAME_RATE)
+    player_x += player_velocity[0] * player_speed
+    player_y += player_velocity[1] * player_speed
+
+    CLOCK.tick(FRAME_RATE)
     pygame.display.update()
