9d33b8888a4465754b662cf43e168238cb47ec61
Dan <jumagaliyevj2004@gmail.com>
2022-11-25 21:54:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,102 @@ BLACK = (0, 0, 0)
 WINDOW_SIZE = (1280, 720)
 WINDOW_TITLE = "Pygame Tutorial"
 
+HORIZONTAL = 1
+UP = 2
+DOWN = 0
+
 FRAME_RATE = 60
+ANIMATION_FRAME_RATE = 10
 
 WINDOW = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption(WINDOW_TITLE)
 
 CLOCK = pygame.time.Clock()
 
-player_x = WINDOW_SIZE[0] / 2
-player_y = WINDOW_SIZE[1] / 2
-player_input = {"left": False, "right": False, "up": False, "down":False}
-player_velocity = [0, 0]
-player_speed = 5
+background = pygame.transform.scale(pygame.image.load("assets/background.png"), WINDOW_SIZE)
+
+objects = []
+
+
+class Object:
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+        self.velocity = [0, 0]
+
+        objects.append(self)
+
+    def draw(self):
+        WINDOW.blit(pygame.transform.scale(self.image, (self.width, self.height)), (self.x, self.y))
+
+    def update(self):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
+        self.draw()
+
+
+class Entity(Object):
+    def __init__(self, x, y, width, height, tileset, speed):
+        super().__init__(x, y, width, height, None)
+        self.speed = speed
+
+        self.tileset = load_tileset(tileset, 16, 16)
+        self.direction = 0
+        self.flipX = False
+        self.frame = 0
+        self.frames = [0, 1, 0, 2]
+        self.frame_timer = 0
+
+    def change_direction(self):
+        if self.velocity[0] < 0:
+            self.direction = HORIZONTAL
+            self.flipX = True
+        elif self.velocity[0] > 0:
+            self.direction = HORIZONTAL
+            self.flipX = False
+        elif self.velocity[1] > 0:
+            self.direction = DOWN
+        elif self.velocity[1] < 0:
+            self.direction = UP
+
+    def draw(self):
+        image = pygame.transform.scale(self.tileset[self.frames[self.frame]][self.direction], (self.width, self.height))
+
+        self.change_direction()
+
+        image = pygame.transform.flip(image, self.flipX, False)
+        WINDOW.blit(image, (self.x, self.y))
+
+        if self.velocity[0] == 0 and self.velocity[1] == 0:
+            self.frame = 0
+            return
+
+        self.frame_timer += 1
+
+        if self.frame_timer < ANIMATION_FRAME_RATE:
+            return
+
+        self.frame += 1
+        if self.frame >= len(self.frames):
+            self.frame = 0
+
+        self.frame_timer = 0
+
+    def update(self):
+        self.x += self.velocity[0] * self.speed
+        self.y += self.velocity[1] * self.speed
+        self.draw()
+
+
+class Player(Entity):
+    def __init__(self, x, y, width, height, tileset, speed):
+        super().__init__(x, y, width, height, tileset, speed)
+
+
+player_input = {"left": False, "right": False, "up": False, "down": False}
 
 
 def check_input(key, value):
@@ -35,6 +119,25 @@ def check_input(key, value):
         player_input["down"] = value
 
 
+def load_tileset(filename, width, height):
+    image = pygame.image.load(filename).convert_alpha()
+    image_width, image_height = image.get_size()
+    tileset = []
+    for tile_x in range(0, image_width // width):
+        line = []
+        tileset.append(line)
+        for tile_y in range(0, image_height // height):
+            rect = (tile_x * width, tile_y * height, width, height)
+            line.append(image.subsurface(rect))
+    return tileset
+
+
+# Objects
+#test_object = Object(400, 400, 50, 50, pygame.image.load("assets/heart.png"))
+#test_entity = Entity(400, 400, 50, 50, "assets/player-Sheet.png", 5)
+player = Player(WINDOW_SIZE[0] / 2, WINDOW_SIZE[1] / 2, 75, 75, "assets/player-Sheet.png", 5)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,18 +147,13 @@ while True:
         elif event.type == pygame.KEYUP:
             check_input(event.key, False)
 
-    player_velocity[0] = player_input["right"] - player_input["left"]
-    player_velocity[1] = player_input["down"] - player_input["up"]
+    player.velocity[0] = player_input["right"] - player_input["left"]
+    player.velocity[1] = player_input["down"] - player_input["up"]
 
-    WINDOW.fill(WHITE)
+    WINDOW.blit(background, (0, 0))
 
-    pygame.draw.rect(WINDOW, RED, (600, 600, 200, 100))
-
-    # Player
-    pygame.draw.circle(WINDOW, BLACK, (player_x, player_y), 50)
-
-    player_x += player_velocity[0] * player_speed
-    player_y += player_velocity[1] * player_speed
+    for obj in objects:
+        obj.update()
 
     CLOCK.tick(FRAME_RATE)
     pygame.display.update()
